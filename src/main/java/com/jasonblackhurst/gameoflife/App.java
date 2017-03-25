ba91956c7b06d367994602b1e288c8ac6580eb0f
jason-blackhurst <jason.blackhurst@mheducation.com>
2017-03-25 17:32:28 -0400

    initial commit of gol simple sample app

---
+++ src/main/java/com/jasonblackhurst/gameoflife/App.java
@@ -0,0 +1,15 @@
+package com.jasonblackhurst.gameoflife;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public boolean testableMethod() { return true; }
+}
---
+++ src/test/java/com/jasonblackhurst/gameoflife/AppTest.java
@@ -0,0 +1,17 @@
+package com.jasonblackhurst.gameoflife;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by jason_blackhurst on 3/25/17.
+ */
+public class AppTest {
+
+    @Test
+    public void testTestableMethod() throws Exception {
+        App app = new App();
+        assert (app.testableMethod());
+    }
+}
