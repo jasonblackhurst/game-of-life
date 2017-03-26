7b7f856a9f3cbf6e96e59b111afbbcad11d235f6
jason-blackhurst <jason.blackhurst@mheducation.com>
2017-03-26 10:08:13 -0400

    implement tick

--- src/main/java/com/jasonblackhurst/gameoflife/Board.java
+++ src/main/java/com/jasonblackhurst/gameoflife/Board.java
@@ -14,6 +14,10 @@ public class Board {
     boolean cells[][] = new boolean[rowCount][columnCount];
 
     public boolean at(int row, int column) {
+        //if it's out of bounds return false
+        if (row < 1 || row > rowCount || column < 1 || column > columnCount){
+            return false;
+        }
         return cells[row-1][column-1];
     }
 
@@ -27,5 +31,44 @@ public class Board {
 
     public int getColumnCount() {
         return columnCount;
+    }
+
+    /**
+     * Moving from one generation to the next is sometimes called the tick (wikipedia)
+     */
+    public void tick() {
+        boolean newCells[][] = new boolean[rowCount][columnCount];
+
+        for (int x = 1; x <= rowCount; x++){
+            for (int y = 1; y <= columnCount; y++){
+                int neighborCount = getNeighborCount(x,y);
+
+                //live cell with 2 or 3 neighbors stays a live cell
+                if (at(x,y) && (neighborCount == 2 || neighborCount == 3)) {
+                    newCells[x-1][y-1] = true;
+                }
+
+                //dead cell with 3 neighbors becomes a live cell
+                if (!at(x,y) && (neighborCount == 3)) {
+                    newCells[x-1][y-1] = true;
+                }
+
+            }
+        }
+
+        cells = newCells;
+    }
+
+    private int getNeighborCount(int row, int column) {
+        int neighborCount = 0;
+        for (int x = row - 1; x <= row + 1; x++) {
+            for (int y = column - 1; y <= column + 1; y++) {
+                //skip the middle
+                if (!(x == row && y == column)){
+                    neighborCount += ( at(x,y) ? 1 : 0);
+                }
+            }
+        }
+        return neighborCount;
     }
 }
--- src/test/java/com/jasonblackhurst/gameoflife/AppTest.java
+++ src/test/java/com/jasonblackhurst/gameoflife/AppTest.java
@@ -27,9 +27,74 @@ public class AppTest {
         app.addRow("........");
         app.addRow("...OO...");
         app.addRow("...OO...");
-        app.addRow("...OO...");
-        app.printBoard();
         assertTrue("Cell at 1,7 should be alive", app.getBoard().at(1,7));
         assertTrue("Cell at 5,4 should be alive", app.getBoard().at(5,4));
+    }
+
+    @Test
+    public void testDyingFromUnderPopulation() {
+        Board board = new Board();
+        board.setAliveAt(1,1);
+        board.tick();
+        assertFalse(board.at(1,1));
+    }
+
+    @Test
+    public void testStayinAliveWith2Neighbors() {
+        Board board = new Board();
+        board.setAliveAt(1,1); //has two neighbors
+        board.setAliveAt(1,2);
+        board.setAliveAt(2,1);
+        board.tick();
+        assertTrue("Cell with 2 neighbors should live", board.at(1,1));
+    }
+
+    @Test
+    public void testStayinAliveWith3Neighbors() {
+        Board board = new Board();
+        board.setAliveAt(1,1); //has three neighbors
+        board.setAliveAt(1,2);
+        board.setAliveAt(2,1);
+        board.setAliveAt(2,2);
+        board.tick();
+        assertTrue("Cell with 3 neighbors should live", board.at(1,1));
+    }
+
+    @Test
+    public void testDyingFromOverCrowding() {
+        int xToTest = 4;
+        int yToTest = 4;
+
+        //test neighbor count 4 - 8
+        for (int livingCountUnderTest = 4; livingCountUnderTest <= 8; livingCountUnderTest++) {
+            Board board = new Board();
+            //set the middle alive
+            board.setAliveAt(xToTest, yToTest);
+
+            //set neighbors until we get to the count under test
+            int livingCount = 0;
+            for (int x = xToTest - 1; x <= xToTest + 1; x++) {
+                for (int y = yToTest - 1; y <= yToTest + 1; y++) {
+                    //skip the middle
+                    if (!(x == xToTest && y == yToTest) && livingCount < livingCountUnderTest) {
+                        board.setAliveAt(x, y);
+                        livingCount++;
+                    }
+                }
+            }
+            board.tick();
+            assertFalse("Cell with " +livingCountUnderTest + " neighbors should die", board.at(xToTest, yToTest));
+        }
+    }
+
+    @Test
+    public void testCellReproduction() {
+        Board board = new Board();
+        //dead cell 1,1 has three neighbors
+        board.setAliveAt(1,2);
+        board.setAliveAt(2,1);
+        board.setAliveAt(2,2);
+        board.tick();
+        assertTrue("Dead cell with 3 neighbors should become a live cell", board.at(1,1));
     }
 }
