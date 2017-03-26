21a12c4391f53a2588c08a7be0901fd03cb85561
jason-blackhurst <jason.blackhurst@mheducation.com>
2017-03-25 22:56:37 -0400

    board guts and printing

--- src/main/java/com/jasonblackhurst/gameoflife/Board.java
+++ src/main/java/com/jasonblackhurst/gameoflife/Board.java
@@ -4,5 +4,26 @@ package com.jasonblackhurst.gameoflife;
  * Created by jason_blackhurst on 3/25/17.
  */
 public class Board {
-    
+
+    boolean cells[][] = new boolean[8][6];
+
+    public boolean at(int x, int y) {
+        return cells[x][y];
+    }
+
+    public void setAliveAt(int x, int y) {
+        cells[x][y] = true;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (boolean row[] : cells){
+            for (boolean value : row){
+                stringBuilder.append(value ? 'O' : '.');
+            }
+            stringBuilder.append(System.lineSeparator());
+        }
+        return stringBuilder.toString();
+    }
 }
--- src/test/java/com/jasonblackhurst/gameoflife/AppTest.java
+++ src/test/java/com/jasonblackhurst/gameoflife/AppTest.java
@@ -10,7 +10,11 @@ import static org.junit.Assert.*;
 public class AppTest {
 
     @Test
-    public void testCreateBoard() {
+    public void testBoard() {
         Board board = new Board();
+        assertFalse("Cell should be dead", board.at(0,0));
+        board.setAliveAt(0,0);
+        assertTrue("Cell should be alive", board.at(0,0));
+        System.out.println(board);
     }
 }
